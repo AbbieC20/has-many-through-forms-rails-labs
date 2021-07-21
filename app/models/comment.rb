d010a828cbd2676e109ccb959a78d8fc408401be
Abbie Cornelius <abbyc@sky.com>
2021-07-21 08:52:18 +0100

    commit tree ed2938fbe2d0ba3256326e0ec594f2aa91983b14

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(attributes)
+    if !attributes[:username].blank? 
+      user = User.find_or_create_by(attributes)
+      self.user = user 
+    end
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,15 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-
+  
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category_attribute.delete_if { |key, value| value.blank? }
+      if !category_attribute.empty?
+        category = Category.find_or_create_by(category_attribute)
+        self.post_categories.build(category: category)
+      end
+    end
+  end
 
 end
